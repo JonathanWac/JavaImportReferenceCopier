8b0ca7857eabb62e749bb52383c47f5173a13e10
Jonathan's Deck <jonathanwachholz@gmail.com>
2022-10-13 17:09:58 -0500

    Skeleton code and example input added

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 
-
-
-
 import os
 import shutil
+
+
+
+
+def runCopyFunction(oldImpStr: str, newImpStr: str):
+    oldImpTokens = parseImportStr(oldImpStr)
+    newImpTokens = parseImportStr(newImpStr)
+    pass
+
+
+def parseImportStr(impStr: str, delimiter: str = "."):
+    tokens = impStr.split(delimiter)
+    tempList = []
+    for elem in tokens:
+        elem = elem.strip()
+        if elem is not "":
+            tempList.append(elem)
+    return tempList
 
 
 def copyFile(oldSrcFileDir: str, newSrcFolderDir: str):
@@ -24,5 +39,6 @@ def copyFile(oldSrcFileDir: str, newSrcFolderDir: str):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pass
-
+    args = ["import com.ge.capital.cfs.lease.json.model.userprofilejson",
+            "import com.myaccounts.user.userservice.model.userprofilejson",
+            "targeDoc.java"]
